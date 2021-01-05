76a0b8735d6c543148771f433b2c06254d1c2db3
alessiogandelli <alessiogandelli99@gmail.com>
2021-01-05 05:35:33 +0100

    refactor

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ import Telegraf from 'telegraf'
 import session from 'telegraf/session.js'
 import Stage from 'telegraf/stage.js'
 import dotenv from 'dotenv'
+import main from './dumpa.js'
 import databaseHelper from './helpers/database.helper.js'
 import fs from 'fs'
 import { get } from 'http'
@@ -11,47 +12,13 @@ import { get } from 'http'
 dotenv.config()
 
 
-async function start(){
-    await databaseHelper.connect()
-}
-
-start();
+main();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const stage = new Stage([ ]);
 bot.use(session()); 
 bot.use(stage.middleware());
-bot.use((ctx) => stats(ctx))
-bot.start();
-
-//bot.command('/addlibro', Stage.enter('addLibro'));
-
+bot.use()
+bot.start((ctx)=> stats());
 bot.launch();
 
-async function stats(ctx){
-    saveUsers();
-
-    saveStats('d8eea')
-    saveStats('SkuZ')
-    saveStats('valzo')
-    saveStats('Cavich')
-
-}
-
-async function saveUsers(){
-    const usercache = fs.readFileSync('./usercache.json' ,'utf8');
-    const users = JSON.parse(usercache);
-    const dbuser = await databaseHelper.getCollection('users').stats();
-    if(dbuser.count == 0)
-        await databaseHelper.getCollection('users').insertMany(users);
-}
-
-async function saveStats(username){
-    const path = './stats/';
-    const user = await databaseHelper.getCollection('users').findOne({"name" : username});
-    const stats = fs.readFileSync( path+ user.uuid +'.json', 'utf8');
-    const parsed = JSON.parse(stats).stats;
-    parsed.timestamp = new Date();
-    await databaseHelper.getCollection(username).insertOne(parsed);
-
-}
